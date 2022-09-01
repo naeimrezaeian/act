b8fa835a3e140614ddf0175247d3648a3a9256e4
mzarsaw <m.zarsaw@yahoo.com>
2022-09-01 12:48:31 +0430

    add camera recording

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,125 +1,187 @@
 <template>
     <div>
-        <video ref="videoRec"  autoplay class="webcam" ></video>
-         </div>
+        <video ref="videoRec" autoplay class="webcam"></video>
+    </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex'
-import axios from "axios"
-    export default {
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
 
-        name:'ACTCamera',
-        data(){
-            return {
-                isRecording: false, 
-                recorder: null, 
-                stream: null,
-                recordedChunks :[],
-                isWebcamera:false,
+import * as base64 from "byte-base64";
+import { mapActions } from 'vuex'
+import axios from "axios"
+export default {
+
+    name: 'ACTCamera',
+    data() {
+        return {
+            isRecording: false,
+            recorder: null,
+            connection: null,
+            stream: null,
+            recordedChunks: [],
+            isWebcamera: false,
+        }
+    },
+    created() {
+        this.connection = new HubConnectionBuilder()
+            .withUrl('http://51.250.106.203:7023/RecordVideoHub', { accessTokenFactory: () => localStorage.getItem("token") })
+            //.withUrl('https://localhost:7035/RecordVideoHub',{ accessTokenFactory: () => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJTdXBlckFkbWluIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6InRlc3QiLCJleHAiOjE2NjE5ODAyNDcsImlzcyI6Imh0dHA6Ly9BQ1QucnUiLCJhdWQiOiJodHRwOi8vSGFuaXNBcHAuY29tIn0.TZf_zvwWc3RMIUHfc0H0WO_VWWGaeQNu3UnvBfqpscM" })
+            .configureLogging(LogLevel.Information)
+            .withAutomaticReconnect()
+            .build()
+
+        this.connection.start().then(() => {
+            console.log("connected")
+            this.connection.on("multipleConnections", () => {
+                console.log("Multiple logins deteced")
+            })
+            this.connection.on("closeApp", () => {
+                console.log("close App")
+            })
+            let self = this;
+            async function handleDataAvailable(event) {
+
+                const ab = await event.data.arrayBuffer();
+                const bytes = new Uint8Array(ab);
+                const ab64 = base64.bytesToBase64(bytes)
+                await self.connection.invoke("UploadVideoStream", ab64)
             }
-        },
-         computed:{
-          
-            },
-         methods:{
-           ...mapActions(['set_isWebcamera']),
-            init(){
-                if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
-                    navigator.mediaDevices.getUserMedia({
-                     video: {
+            if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+                navigator.mediaDevices.getUserMedia({
+                    video: {
                         width: { ideal: 640 },
                         height: { ideal: 480 }
-                     },
-                     audio: false
-                    }).then(mediaStream=>{
-                        
-                      this.stream=mediaStream;
-                        this.recorder = new MediaRecorder(mediaStream, {
-                          mimeType: "video/webm; codecs=vp9",
-                            audioBitsPerSecond: 128000
-                        });
-                      
-                       this.recorder.ondataavailable = (event) => { this.recordedChunks.push(event.data); };
-                        this.recorder.start(100);
-                        
-                       
-                       this.$refs.videoRec.src = null;
-                       this.$refs.videoRec.srcObject = mediaStream;
-                       this.webcamera=true
-                       this.set_isWebcamera(true)
-                        
+                    },
+                    audio: false
+                }).then(mediaStream => {
+
+                    this.stream = mediaStream;
+                    this.recorder = new MediaRecorder(mediaStream, {
+                        mimeType: "video/webm; codecs=vp9",
+                        audioBitsPerSecond: 128000
+                    });
+
+                    this.recorder.ondataavailable = handleDataAvailable
+                    this.recorder.start(60);
+
+
+                    this.$refs.videoRec.src = null;
+                    this.$refs.videoRec.srcObject = mediaStream;
+                    this.webcam = "OK"
+
+                })
+                    .catch((err) => {
+                        console.log(err)
+
+
+                        this.webcam = "Webcam Error"
                     })
+
+
+            } else {
+                this.webcam = "Webcam unsuported"
+            }
+        })
+        this.init()
+
+    },
+    computed: {
+
+    },
+    methods: {
+        ...mapActions(['set_isWebcamera']),
+        init() {
+            if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+                navigator.mediaDevices.getUserMedia({
+                    video: {
+                        width: { ideal: 640 },
+                        height: { ideal: 480 }
+                    },
+                    audio: false
+                }).then(mediaStream => {
+
+                    this.stream = mediaStream;
+                    this.recorder = new MediaRecorder(mediaStream, {
+                        mimeType: "video/webm; codecs=vp9",
+                        audioBitsPerSecond: 128000
+                    });
+
+                    this.recorder.ondataavailable = (event) => { this.recordedChunks.push(event.data); };
+                    this.recorder.start(100);
+
+
+                    this.$refs.videoRec.src = null;
+                    this.$refs.videoRec.srcObject = mediaStream;
+                    this.webcamera = true
+                    this.set_isWebcamera(true)
+
+                })
                     .catch(() => (
                         console.log("Error media ")
-                       
-                        ));
 
-                            
-                      
-                }else{
-                    alert("can not get media devicess")
-                }
+                    ));
 
-            },
-     
-    
-     async download() {
-this.recorder.stop();
-this.isRecording = false;
-  this.stream.getTracks().forEach(track => {console.log(track); track.stop(); });
 
-var blob = new Blob(this.recordedChunks, {type: "video/webm"});
 
-const headers = {  'Content-Type': 'multipart/form-data'}
-const file = new File ([blob],"test"+new Date().getTime()+".webm",{type:blob.type,lastModified:new Date().getTime()})
-
-let formData=new FormData();
-formData.append('file',file)
-
-   const response= await axios.post("upload",formData,{headers:headers})
-   console.log(response.data)
- // var url =  URL.createObjectURL(blob);
-  //var a = document.createElement("a");
-  //document.body.appendChild(a);
-  //a.style = "display: none";
-  //a.href = url;
-  //a.download = 'test.webm';
-  //a.click();
-  
- // setTimeout(function() { URL.revokeObjectURL(url); }, 100); 
-},
-stopRecord(){
-    console.log("camera stop")
- this.recorder.stop();
-    this.isRecording = false;  
-    this.stream.getTracks().forEach(track => { track.stop(); });
- this.set_isWebcamera(false)
-}
-
-        },beforeMount(){
-           
-           this.init();         
-        },created(){
-          
-        },watch:{
-            stream:function(){
-               // console.log(this.stream)
+            } else {
+                alert("can not get media devicess")
             }
+
+        },
+
+
+        async download() {
+            this.recorder.stop();
+            this.isRecording = false;
+            this.stream.getTracks().forEach(track => { console.log(track); track.stop(); });
+
+            var blob = new Blob(this.recordedChunks, { type: "video/webm" });
+
+            const headers = { 'Content-Type': 'multipart/form-data' }
+            const file = new File([blob], "test" + new Date().getTime() + ".webm", { type: blob.type, lastModified: new Date().getTime() })
+
+            let formData = new FormData();
+            formData.append('file', file)
+
+            const response = await axios.post("upload", formData, { headers: headers })
+            console.log(response.data)
+            // var url =  URL.createObjectURL(blob);
+            //var a = document.createElement("a");
+            //document.body.appendChild(a);
+            //a.style = "display: none";
+            //a.href = url;
+            //a.download = 'test.webm';
+            //a.click();
+
+            // setTimeout(function() { URL.revokeObjectURL(url); }, 100); 
+        },
+        stopRecord() {
+            console.log("camera stop")
+            this.recorder.stop();
+            this.isRecording = false;
+            this.stream.getTracks().forEach(track => { track.stop(); });
+            this.set_isWebcamera(false)
         }
-        
-        
+
+    }, beforeMount() {
+
+        this.init();
+    }, watch: {
+        stream: function () {
+            // console.log(this.stream)
+        }
     }
+
+
+}
 </script>
 
 <style lang="scss" scoped>
-
-.webcam{
-width: 100%;
-margin: -2rem auto;  
-border: 2px solid #ffff;
-border-radius: 20px;
+.webcam {
+    width: 100%;
+    margin: -2rem auto;
+    border: 2px solid #ffff;
+    border-radius: 20px;
 }
-
-
 </style>
--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,25 +1,33 @@
  /* eslint-disable */
 <template>
-<transition name="fade">
-<div class="lds-wrapper" v-if="visible">
-<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-</div>
-</transition>
+  <transition name="fade">
+    <div class="lds-wrapper" v-if="visible">
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 
 <script>
 
-    export default {
-      name:'preloader',
-	
-	 props: ["visible"]
-	 
-    }
+export default {
+  name: 'preLoader',
+
+  props: ["visible"]
+
+}
 </script>
 
 <style scoped>
-
 .lds-wrapper {
   background: lightgrey;
   position: absolute;
@@ -28,19 +36,22 @@
   right: 0;
   left: 0;
   display: grid;
-  
+
   place-items: center;
 }
+
 .lds-roller {
   display: inline-block;
   position: relative;
   width: 80px;
   height: 80px;
 }
+
 .lds-roller div {
   animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
   transform-origin: 40px 40px;
 }
+
 .lds-roller div:after {
   content: " ";
   display: block;
@@ -51,71 +62,86 @@
   background: rgb(77, 37, 255);
   margin: -4px 0 0 -4px;
 }
+
 .lds-roller div:nth-child(1) {
   animation-delay: -0.036s;
 }
+
 .lds-roller div:nth-child(1):after {
   top: 63px;
   left: 63px;
 }
+
 .lds-roller div:nth-child(2) {
   animation-delay: -0.072s;
 }
+
 .lds-roller div:nth-child(2):after {
   top: 68px;
   left: 56px;
 }
+
 .lds-roller div:nth-child(3) {
   animation-delay: -0.108s;
 }
+
 .lds-roller div:nth-child(3):after {
   top: 71px;
   left: 48px;
 }
+
 .lds-roller div:nth-child(4) {
   animation-delay: -0.144s;
 }
+
 .lds-roller div:nth-child(4):after {
   top: 72px;
   left: 40px;
 }
+
 .lds-roller div:nth-child(5) {
   animation-delay: -0.18s;
 }
+
 .lds-roller div:nth-child(5):after {
   top: 71px;
   left: 32px;
 }
+
 .lds-roller div:nth-child(6) {
   animation-delay: -0.216s;
 }
+
 .lds-roller div:nth-child(6):after {
   top: 68px;
   left: 24px;
 }
+
 .lds-roller div:nth-child(7) {
   animation-delay: -0.252s;
 }
+
 .lds-roller div:nth-child(7):after {
   top: 63px;
   left: 17px;
 }
+
 .lds-roller div:nth-child(8) {
   animation-delay: -0.288s;
 }
+
 .lds-roller div:nth-child(8):after {
   top: 56px;
   left: 12px;
 }
+
 @keyframes lds-roller {
   0% {
     transform: rotate(0deg);
   }
+
   100% {
     transform: rotate(360deg);
   }
 }
-
-
-
 </style>
